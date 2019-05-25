733c87d0739aed712cb54a36e00d39c39d95f892
matthew.serre <matthew.serre@gmail.com>
2019-05-25 17:54:17 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
     Show.minimum(:rating)
   end
   
-  def least_popular_show
+  def self.least_popular_show
     Show.find_by(rating: self.lowest_rating)
   end
   
