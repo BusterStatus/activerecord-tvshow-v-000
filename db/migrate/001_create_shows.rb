815f003be5f37ffa58c969c44d64dbdfd2c8db6f
matthew.serre <matthew.serre@gmail.com>
2019-05-25 17:26:17 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,11 @@
 Class Shows < ActiveRecord::Migration[4.2]
 
-  create_table :shows do |t|
-    t.string :name
-    t.string :network
-    t.string :day
-    t.integer :rating
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
   end
   
 end
